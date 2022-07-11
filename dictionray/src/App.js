73cac19b0615b251c02735c842e27e1e2bad8abd
iammansura <mansura.akter091@gmail.com>
2022-07-11 15:06:27 +0200

    set logo

--- dictionray/src/App.js
+++ dictionray/src/App.js
@@ -1,13 +1,13 @@
-import img from './logo'
+import logo from './dictionary.png'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={img} className="App-logo" alt="logo" />
-        <h1>Haloo</h1> hallo
+      <header>
+        <img src={logo} alt="Dictionary-logo" className="logo " />
       </header>
+      <h1>Hallo</h1>
     </div>
   )
 }
