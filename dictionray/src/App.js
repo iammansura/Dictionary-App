62bd08e432680302c7275d6bcd691dd3967581af
iammansura <mansura.akter091@gmail.com>
2022-07-11 14:47:51 +0200

    fixed

---
+++ dictionray/src/App.js
@@ -0,0 +1,15 @@
+import img from './logo'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} className="App-logo" alt="logo" />
+        <h1>Haloo</h1> hallo
+      </header>
+    </div>
+  )
+}
+
+export default App
