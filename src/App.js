3f938ebebe122af042a3773fde8cf2bfd312b78f
iammansura <mansura.akter091@gmail.com>
2022-07-11 22:14:12 +0200

    setup app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './dictionary.png'
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} alt="Dictionary-logo" className="logo " />
+        <img src={logo} alt="Dictionary-logo" className="logo img-fluid " />
       </header>
-      <h1>Hallo</h1>
+      <h1 className="fs-1">Dictionary</h1>
     </div>
   )
 }
