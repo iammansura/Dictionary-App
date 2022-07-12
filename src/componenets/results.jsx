a86330fada5a3bf55f4aa41eb9044a8965ff39ac
iammansura <mansura.akter091@gmail.com>
2022-07-12 18:41:19 +0200

    added phonetics

--- src/componenets/results.jsx
+++ src/componenets/results.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Meaning from './meaning'
+import Phonetic from './phonetic'
+
 export default function Reasults(props) {
   if (props.reasult) {
     return (
       <div className="reasults">
         <h2>{props.reasult.word}</h2>
+        {props.reasult.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          )
+        })}
         {props.reasult.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
