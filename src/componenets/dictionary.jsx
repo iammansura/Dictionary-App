5c939eef88b5024d82c048a90c681e1369aa9ac0
iammansura <mansura.akter091@gmail.com>
2022-07-12 10:06:58 +0200

    added axios and api for dictionary

--- src/componenets/dictionary.jsx
+++ src/componenets/dictionary.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import './../styles/dictionary.css'
+
+//https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
 export default function Dictionary() {
   let [value, setValue] = useState(' ')
 
+  function handleResponse(response) {
+    console.log(response.data[0])
+  }
+
   function searchForm(event) {
     event.preventDefault()
     alert(`Searching for ${value}`)
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`
+    axios.get(apiUrl).then(handleResponse)
   }
+
   function updatevalue(event) {
     setValue(event.target.value)
   }
